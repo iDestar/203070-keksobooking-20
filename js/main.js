ce5d1a80af2de958c45da6cd97b011f5a9059572
iDestar <destarpwnz@gmai.com>
2020-07-03 14:51:14 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -267,17 +267,17 @@ adFormGuestNumber.addEventListener('change', function () {
 var mapPinsCard = document.querySelectorAll('.map__pin');
 
 
-var elc = function (element, data) {
-  element.addEventListener('click', function () {
-    map.insertBefore(getCard(data), mapFilterContainer);
-  });
-};
+// var elc = function (element, data) {
+// element.addEventListener('click', function () {
+//  map.insertBefore(getCard(data), mapFilterContainer);
+// });
+// };
 
-var openPopupCard = function (pins, arr) {
-  for (var i = 0; i < mapPinsCard.length; i++) {
-    elc(pins[i + 1], arr[i]);
-  }
-};
+// var openPopupCard = function (pins, arr) {
+//  for (var i = 0; i < mapPinsCard.length; i++) {
+//    elc(pins[i + 1], arr[i]);
+//  }
+// };
 
 openPopupCard(mapPinsCard, adMap);
 
